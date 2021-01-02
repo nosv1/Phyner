f1ebd0a9b98af24ffb903fbb5c327c8f1bcb58e2
Chris Seagraves <christopher.seagraves@gmail.com>
2021-01-02 10:10:45 -0600

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -608,7 +608,7 @@ async def startup():
 
     await client.change_presence(
         activity=discord.Activity(
-            type=discord.ActivityType.playing, name="[WIP] @Phyner is finer."
+            type=discord.ActivityType.playing, name="Alpha testing..."
         ),
         status=discord.Status.online
     )
