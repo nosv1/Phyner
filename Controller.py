0206b32d4255b75e89d6a880f3ad540ebe50da7b
Chris Seagraves <christopher.seagraves@gmail.com>
2021-03-17 13:28:54 -0500

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -424,11 +424,11 @@ async def on_raw_reaction_add(payload):
 
         phyner = Support.get_phyner_from_channel(message.channel)
 
-        remove_reaction_bool = False
+        remove_reaction = False
 
-        async def remove_reaction(remove_reaction_bool):
-            if remove_reaction_bool and not is_dm:
-                await message.remove_reaction_bool(payload.emoji, user)
+        async def remove_reaction_check(remove_reaction):
+            if remove_reaction and not is_dm:
+                await message.remove_reaction(payload.emoji, user)
         # end remove_reaction
 
 
@@ -458,9 +458,9 @@ async def on_raw_reaction_add(payload):
                         event.condition.id == message.id,
                         str(event.object.id) == str(payload.emoji)
                     ]):
-                        remove_reaction_bool = await Events.perform_action(client, message, user, event)
+                        remove_reaction = await Events.perform_action(client, message, user, event)
                 
-                await remove_reaction(remove_reaction_bool)
+                await remove_reaction_check(remove_reaction)
 
 
                 ## PHYNER REACTION REMOVE CHECKS 
@@ -504,9 +504,9 @@ async def on_raw_reaction_add(payload):
 
 
                     if message.guild.id == COTM.cotm_id: # COTM
-                        remove_reaction_bool = await COTM.on_reaction_add(client, message, user, payload)
+                        remove_reaction = await COTM.on_reaction_add(client, message, user, payload)
                 
-                await remove_reaction(remove_reaction_bool)
+                await remove_reaction_check(remove_reaction)
 
 
                 ## PHYNER AUTHOR ##
@@ -520,10 +520,10 @@ async def on_raw_reaction_add(payload):
                         if poss_table:
                             await message.add_reaction(Support.emojis._9b9c9f_emoji)
                             await poss_table[0].send_table(client)
-                            await Support.remove_reaction_bools(message, phyner, Support.emojis._9b9c9f_emoji)
-                            remove_reaction_bool = True
+                            await Support.remove_reactions(message, phyner, Support.emojis._9b9c9f_emoji)
+                            remove_reaction = True
                 
-                await remove_reaction(remove_reaction_bool)
+                await remove_reaction_check(remove_reaction)
 
 
     
