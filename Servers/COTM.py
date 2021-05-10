6e946410f2ebade4fc75825c9d199f53c95c9bcf
Chris Seagraves <christopher.seagraves@gmail.com>
2021-05-10 13:06:26 -0500

    Update COTM.py

--- Servers/COTM.py
+++ Servers/COTM.py
@@ -1960,7 +1960,7 @@ async def update_divisions(guild, roster_ws=None):
     roster = roster_ws.get(f"C4:G{roster_ws.row_count}")
 
     
-    div_channels = [c for c in guild.channels if re.findall(rf"(division-[1-{num_divs}])|(waiting-list)", c.name)]
+    div_channels = [c for c in guild.channels if re.findall(rf"(division-[1-{8}])|(waiting-list)", c.name)]
     div_channels.sort(key=lambda x:x.name)
     div_roles = [r for r in guild.roles if re.findall(rf"(^Division [1-{8}]$)|(^Waiting List$)", r.name)]
 
