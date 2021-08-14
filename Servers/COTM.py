d0864d5d803df1fe40a0be480c74382e5c2c6aa9
Chris Seagraves <christopher.seagraves@gmail.com>
2021-08-14 09:53:13 -0500

    Update COTM.py

--- Servers/COTM.py
+++ Servers/COTM.py
@@ -3,7 +3,7 @@
 import asyncio
 from datetime import datetime
 import discord
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 # import matplotlib as mpl
 # import numpy as np
 import os
